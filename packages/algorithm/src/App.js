567dedd60f95279aa8a8613144ac535def742e02
택준 <taekjunsim@gmail.com>
2024-06-27 20:31:33 +0900

    버블정렬 구현

--- packages/algorithm/src/App.js
+++ packages/algorithm/src/App.js
@@ -1,9 +1,11 @@
 import { insertionSort } from "util/sorts/insertionSort";
 import { selectionSort } from "util/sorts/selectionSort";
+import { bubbleSort } from "util/sorts/bubbleSort";
 
 export default function App() {
   insertionSort([4, 3, 2, 1, 5, -5, 20, 17]);
   selectionSort([4, 3, 2, 1, 5, -5, 20, 17]);
+  bubbleSort([4, 3, 2, 1, 5, -5, 20, 17]);
 
   return <div>Algorithm Packages</div>;
 }
