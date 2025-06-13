0169974827e3b9cab4ce6fd6cc19e3ffb7096800
Larry <taekjunsim@gmail.com>
2025-06-13 15:13:49 +0900

    chore: booking-movie-ticket 주석 추가

--- packages/algorithm/src/sliding-window/booking-movie-ticket.js
+++ packages/algorithm/src/sliding-window/booking-movie-ticket.js
@@ -1,3 +1,8 @@
+// https://www.acmicpc.net/problem/29700
+/*
+나란히 앉을 수 있는 최대 좌석 수를 구하는 경우는 범위가 가변적이기 때문에 투포인터를 사용하는 것이 적합
+*/
+
 // 가로줄(m)이 동아리원 수(k)보다 큰 경우
 // 가로줄(m)이 동아리원 수(k)보다 작은 경우
 
@@ -49,3 +54,5 @@ const caculateCases = (seats, columns, rows, numbers) => {
 
 const cases = caculateCases(seats, n, m, k);
 console.log(cases);
+
+
